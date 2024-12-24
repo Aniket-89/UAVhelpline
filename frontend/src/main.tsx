18190a89e1af30c320111872d8b6ae466c418783
Aniket <aniketkumarg99@gmail.com>
2024-12-24 18:44:09 +0530

    minor type fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import AOS, { AosOptions } from "aos";
 import "aos/dist/aos.css";
-import About from "./pages/About";
-import BlogList from "./pages/BlogList";
-import Industry from "./pages/Industry";
-import BlogDetailPage from "./pages/BlogDetailPage";
+
 import BaseLayout from "./Layouts/BaseLayout";
 import Index from "./pages/Index";
 
@@ -18,10 +15,6 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Index />,
-      },
-      {
-        path: "about/",
-        element: <About />,
       },
     //   {
     //     path: "blogs/",
