b9f5955e3c4ae3c85f7de4917ed5062e61dff30b
Aniket <aniketkumarg99@gmail.com>
2024-12-29 19:34:42 +0530

    docker setup

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -1,14 +1,19 @@
 from .base import *
+import os
 
 DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT', '5432'),
+#     }
+# }
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
